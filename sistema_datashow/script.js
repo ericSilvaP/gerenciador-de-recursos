821e30e37f15dcdf70a2cd1702bfb656a75d0f6f
Eric Silva <ericsilvap13@gmail.com>
2024-09-24 16:45:21 -0300

    menu alt, org tabela, pag agendados

--- sistema_datashow/script.js
+++ sistema_datashow/script.js
@@ -1,13 +1,50 @@
-let tabRec = document.querySelector("table")
-let linhaRec = document.querySelector("table > tr")
-let celRec = document.querySelector("table > tr > td")
+var a = document.getElementsByClassName("alt")
 
-function atual(){
-    for (let c = 1; c <= 11; c++) {
-        /*if (document.querySelector(`linha${c} > td`).innerText == "Disponível") {
-            document.querySelector(`linha${c} > td`).style.backgroundColor = "green"
-        }*/
-       document.querySelector(`linha${c} > td`).style.backgroundColor = "green"
-       document.querySelector("main").style.backgroundColor = "green"
+a[0].dataset.content = "▲"
+a[1].dataset.content = "▲"
+a[2].dataset.content = "▲"
+
+function altTitle01() {
+    if (altItems01.style.display == "block") {
+        altItems01.style.display = "none"
+        a[0].dataset.content = "▼"
+    } else {
+        altItems01.style.display = "block"
+        a[0].dataset.content = "▲"
+    }
+}
+
+function altTitle02() {
+    if (altItems02.style.display == "block") {
+        altItems02.style.display = "none"
+        a[1].dataset.content = "▼"
+    } else {
+        altItems02.style.display = "block"
+        a[1].dataset.content = "▲"
+    }
+}
+
+function altTitle03() {
+    if (altItems03.style.display == "block") {
+        altItems03.style.display = "none"
+        a[2].dataset.content = "▼"
+    } else {
+        altItems03.style.display = "block"
+        a[2].dataset.content = "▲"
+    }
+}
+
+function clickTr() {
+    let p = document.querySelectorAll("tr")
+    for (let c = 2; c <= 9; c++) {
+        
+        if (p[3].value){
+            window.alert("c")
+            p[3].value.value = 0
+            p[3].value.style.backgroundColor = "lightgreen"
+        } else {
+            p[3].value.value = 1
+            p[3].value.style.backgroundColor = "green"
+        }
     }
 }
